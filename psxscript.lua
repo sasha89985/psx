7f1f127fc255cbd73d441dafe8fe387cbbe81b2e
sasha89985 <123018833+sasha89985@users.noreply.github.com>
2023-01-18 22:35:35 +0300

    Create psxscript.lua

---
+++ psxscript.lua
@@ -0,0 +1,67 @@
+-- Ссылка на Библиотеку
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+--[[ 
+В данный момент стоит тема "RJTheme3" ,
+вы можете использовать другую тему приведённую ниже -
+"RJTheme1"
+"RJTheme2"
+"RJTheme3"
+"RJTheme4"
+"RJTheme5"
+"RJTheme6"
+"RJTheme7"
+"RJTheme8"
+//////////////////////////////////////////////////////////////////
+
+Что бы сделать свою тему , уберите часть скрипта из "комминтариев" ,
+который находится чуть ниже , и вместо "RJTheme3" в переменной "Windows" - 
+напишите переменную которая используется в скрипте чуть ниже .
+]]
+--[[
+local colors = {
+	-- Цвет фона у Секций
+    SchemeColor = Color3.fromRGB(150, 72, 148),
+	-- Цвет фона в правой части UI
+	Background = Color3.fromRGB(15,15,15),
+	-- Цвет фона в левой части UI
+    Header = Color3.fromRGB(15,15,15),
+	-- Цвет текста
+    TextColor = Color3.fromRGB(255,255,255),
+	-- Цвет фона у кнопок
+    ElementColor = Color3.fromRGB(20, 20, 20)
+}
+]]
+-- Создать окно UI
+local Window = Library.CreateLib("vladik_pedik", "RJTheme3")
+
+-- Секция
+local Tab = Window:NewTab("semiya vlada")
+
+-- Подсекция
+local Section = Tab:NewSection("traxnut vlada")
+
+local Section = Tab:NewSection("mama vlada")
+
+-- Заголовок
+Section:NewLabel("xui")
+
+-- Кнопка
+Section:NewButton("farm coins", "ButtonInfo", function()
+    print("Clicked")
+
+end)
+
+-- Переключатель
+Section:NewToggle("ToggleText", "ToggleInfo", function(state)
+    if state then
+        print("Toggle On")
+    else
+        print("Toggle Off")
+    end
+end)
+
+-- Слайдер
+Section:NewSlider("skorostb vladaw", "SliderInfo", 500, 0, function(s) -- 500 (Макс. значение) | 0 (Мин. значение)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+
